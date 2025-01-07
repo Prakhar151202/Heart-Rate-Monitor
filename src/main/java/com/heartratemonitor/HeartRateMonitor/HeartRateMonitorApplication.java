53c362368ea9ac48ea20571ec02604499df3dbfd
Prakhar_151202 <prakhar.15december@gmail.com>
2025-01-08 00:42:52 +0530

    init commit

---
+++ src/main/java/com/heartratemonitor/HeartRateMonitor/HeartRateMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.heartratemonitor.HeartRateMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartRateMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartRateMonitorApplication.class, args);
+	}
+
+}
