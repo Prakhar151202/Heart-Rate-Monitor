22937f62a0948b98a8e0806705e3dce15012522c
Prakhar_151202 <prakhar.15december@gmail.com>
2025-01-10 15:46:34 +0530

    init commit

--- src/main/java/com/heartratemonitor/HeartRateMonitor/controller/UserController.java
+++ src/main/java/com/heartratemonitor/HeartRateMonitor/controller/UserController.java
@@ -24,31 +24,30 @@ public class UserController {
 
     @PostMapping("/registerUser")
     public ResponseEntity<String> addUser(@Valid @RequestBody Users user) {
-        // Check if user with the given email already exists
+        // Validation check for unique email
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             return new ResponseEntity<>("Email already exists", HttpStatus.BAD_REQUEST);
         }
 
-        // Save the user to the database with plain text password
+        // Add the user to the database
         userRepository.save(user);
         return new ResponseEntity<>("New User Added Successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> loginUser(@RequestBody Users loginRequest) {
-        // Check if user exists
+        // Check for existence of user
         Optional<Users> optionalUser = userRepository.findByEmail(loginRequest.getEmail());
         if (!optionalUser.isPresent()) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
         }
 
-        // Validate password
+        // Password Validation
         Users user = optionalUser.get();
         if (!user.getPassword().equals(loginRequest.getPassword())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
         }
 
-        // Login success
         return ResponseEntity.ok("Login successful");
     }
 }
